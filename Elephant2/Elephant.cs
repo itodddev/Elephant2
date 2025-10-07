de05b1d77477cd57fba35375d7e742307f34cf19
Todd James <todd@itodd.co>
2025-10-06 21:32:29 -0400

    Add project files.

---
+++ Elephant2/Elephant.cs
@@ -0,0 +1,15 @@
+﻿namespace ElephantSwap
+{
+    public class Elephant
+    {
+        public string? Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My ears are " + EarSize + " inches tall");
+        }
+
+    }
+}
---
+++ Elephant2/Program.cs
@@ -0,0 +1,31 @@
+﻿using ElephantSwap;
+
+Elephant lloyd = new Elephant { Name = "Lloyd", EarSize = 40 };
+Elephant lucinda = new Elephant { Name = "Lucinda", EarSize = 33 };
+
+
+Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+
+while (true)
+{
+    int value = Console.Read();
+    if (value == '1')
+    {
+        Console.WriteLine("You pressed 1");
+        lloyd.WhoAmI();
+    }
+    else if (value == '2')
+    {
+        Console.WriteLine("You pressed 1");
+        lucinda.WhoAmI();
+    }
+    else if (value == '3')
+    {
+        Console.WriteLine("You pressed 3");
+        Console.WriteLine("References have been swapped");
+        Elephant holder;
+        holder = lloyd;
+        lloyd = lucinda;
+        lucinda = holder;
+    }
+}
